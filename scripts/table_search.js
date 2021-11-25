c30acec95d3a049e77f4b09d59304f06a9d251d1
lkaune85 <93952409+lkaune85@users.noreply.github.com>
2021-11-25 15:54:30 -0500

    javascript search code added, updated navbar, new keyword page added
    -added js code used to search tables
    -updated navagation bar to include dropdown menus and changed some styles
    -added missing citations

--- scripts/table_search.js
+++ scripts/table_search.js
@@ -1,24 +1,25 @@
 /*
 *Code adapted from w3schools filter/search table example
 *https://www.w3schools.com/howto/howto_js_filter_table.asp
+*Cited November 24, 2021
 */
 function searchTable() {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("keySearch");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("keywords");
-    tr = table.getElementsByTagName("tr");
+    input = document.getElementById("keySearch"); //get search input
+    filter = input.value.toUpperCase(); //convert input to uppercase for non-case-sensitive filter
+    table = document.getElementById("keywords"); //get table
+    tr = table.getElementsByTagName("tr"); //get table data
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
+        td = tr[i].getElementsByTagName("td")[0]; //get table data from first column[0] and ith row
+        if (td) { //if data is not empty
+        txtValue = td.textContent || td.innerText; //get data content or inner text if no content present
+        if (txtValue.toUpperCase().indexOf(filter) > -1) { //if the text contains the filter
+            tr[i].style.display = ""; //display text
         } else {
-            tr[i].style.display = "none";
+            tr[i].style.display = "none"; //do not display text
         }
         }
     }
