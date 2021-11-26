0b6581671811f4fde2e9e6942d461e9a884bc9b2
lkaune85 <93952409+lkaune85@users.noreply.github.com>
2021-11-26 13:00:12 -0500

    minor edits
    -fixed indents

--- scripts/table_search.js
+++ scripts/table_search.js
@@ -13,14 +13,14 @@ function searchTable() {
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0]; //get table data from first column[0] and ith row
+        td = tr[i].getElementsByTagName("td")[0]; //get table data from first column [0] and ith row
         if (td) { //if data is not empty
-        txtValue = td.textContent || td.innerText; //get data content or inner text if no content present
-        if (txtValue.toUpperCase().indexOf(filter) > -1) { //if the text contains the filter
-            tr[i].style.display = ""; //display text
-        } else {
-            tr[i].style.display = "none"; //do not display text
-        }
+            txtValue = td.textContent || td.innerText; //get data content or inner text if no content present
+            if (txtValue.toUpperCase().indexOf(filter) > -1) { //if the text contains the filter
+                tr[i].style.display = ""; //display text
+            } else {
+                tr[i].style.display = "none"; //do not display text
+            }
         }
     }
 }
